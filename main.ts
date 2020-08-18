b4c942bf01e19b27e524dcafad7bf0f43c4adfd5
Windrun <fengrun@gmail.com>
2020-08-19 03:28:58 +0800

    Added opening and closing messages on OLED

--- main.ts
+++ main.ts
@@ -17,10 +17,21 @@ tinkercademy.crashSensorSetup(DigitalPin.P16)
 OLED.drawLoading(Math.map(servoangle, 90, 175, 0, 100))
 basic.forever(function () {
     if (tinkercademy.PIR(DigitalPin.P13)) {
+        servos.P2.setAngle(90)
+        OLED.writeStringNewLine("Opening")
+        basic.pause(1000)
+        OLED.clear()
+        basic.showIcon(IconNames.Yes)
+        OLED.writeStringNewLine("Enter")
+        basic.pause(1000)
+        OLED.clear()
+        OLED.writeStringNewLine("Closing")
+        basic.showIcon(IconNames.No)
         while (!(tinkercademy.crashSensor()) && servoangle < 175) {
+            music.playTone(262, music.beat(BeatFraction.Whole))
             servoangle += 5
             servos.P2.setAngle(Math.constrain(servoangle, 90, 175))
-            basic.pause(100)
+            basic.pause(150)
             OLED.drawLoading(Math.map(servoangle, 90, 175, 0, 100))
         }
     }
